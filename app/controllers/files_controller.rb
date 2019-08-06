8c38687f9d5f53f0f40f35fb667d443add3fec6e
John Broderick <johnbroderick12@gmail.com>
2019-08-06 15:21:57 -0400

    rails is the most disgusting, bloated framework I have ever had the displeasure of using

--- app/controllers/files_controller.rb
+++
@@ -1,7 +0,0 @@
-class FilesController < ApplicationController
-	def new
-	end
-	def create
-			render plain: params[:file].inspect
-	end
-end
--- app/controllers/pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class PagesController < ApplicationController
-	def about
-		@title = 'About This App'
-		@content = 'This is the about page'
-	end
-end
